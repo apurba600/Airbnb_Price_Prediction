de75089e3cdd5ac17683f1bedbcae38511e531db
Apurba Pandey <apand024@uottawa.ca>
2023-11-28 19:57:01 -0500

    Update streamlit_app.py

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
-with open('style.css') as f:
+with open('streamlit/style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
 st.sidebar.header('Airbnb Price Prediction')
